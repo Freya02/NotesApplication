6fb884dc8c1d0f5154fe927eae032053de788c29
FREYA <iamfreyavora@gmail.com>
2018-06-27 21:41:03 +0530

    First Commit

---
+++ app/src/main/java/com/example/freya/notesapplication/DetailedNote.java
@@ -0,0 +1,13 @@
+package com.example.freya.notesapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_note);
+    }
+}
